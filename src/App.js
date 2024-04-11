59e7af0ca4b255fec8f57961d54480a0b6504e48
Albert <casco_alberto015@hotmail.com>
2024-04-11 12:09:44 -0400

    Primera modificacion de HTML

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Soy Alberto y <code>Este es</code> My app React.
         </p>
         <a
           className="App-link"
